bf6f2c1313a780a5ee2ea542b7e050237ac667d1
Defal7 <mth.fe.a@protonmail.com>
2021-03-04 16:22:23 -0300

    Added getops to start-kvm.sh

--- start-kvm.sh
+++ start-kvm.sh
@@ -3,8 +3,28 @@ source qemu.config
 
 OS=Windows
 
-QEMUCMD= 
-  qemu-system-x86_64 -enable-kvm -boot menu=on \
+while getopts ":s:-h" opt; do
+  case $opt in
+    h)
+      echo "-s [SYSTEM] >> name of the screen used for the os"  
+      echo "-h >> fucking guess."
+      exit 1
+      ;;
+    s)
+      OS=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "-f requires an file."
+      echo "Parameter: -f /path/to/file"
+      exit 1
+      ;;
+  esac
+done
+
+QEMUCMD= qemu-system-x86_64 -enable-kvm -boot menu=on \
     -drive file=$file.img -m $ram -cpu host -smp $threads \
     -vga virtio -display sdl.gl=on
 
